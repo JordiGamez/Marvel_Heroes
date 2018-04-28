5ff077818b399e0871d5da46d44f977d79bd9c31
Jordi Gamez <jordigamezllorach@gmail.com>
2018-04-28 10:36:35 +0200

    Initial Commit

---
+++ Marvel Heroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marvel Heroes
+//
+//  Created by Jordi Gamez on 28/4/18.
+//  Copyright © 2018 Jordi Gamez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Marvel HeroesTests/Marvel_HeroesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Marvel_HeroesTests.swift
+//  Marvel HeroesTests
+//
+//  Created by Jordi Gamez on 28/4/18.
+//  Copyright © 2018 Jordi Gamez. All rights reserved.
+//
+
+import XCTest
+@testable import Marvel_Heroes
+
+class Marvel_HeroesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
